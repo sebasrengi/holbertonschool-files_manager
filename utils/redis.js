c351ed878a85f10490bd235f451efc0905e3d124
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2023-02-05 12:36:58 -0500

    Update redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -2,32 +2,33 @@ const redis = require('redis');
 const { promisify } = require('util');
 
 class RedisClient {
-    constructor() {
-	this.client = redis.createClient();
-	this.getAsync = promisify(this.client.get).bind(this.client);
+  constructor() {
+    this.client = redis.createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
 
-	this.client.on('error', (error) => {
-	    console.log(`Redis client not connected to the server: ${error}`);
-	});
-    }
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to the server: ${error}`);
+    });
+  }
 
-    isAlive() {
-	return this.client.connected;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
-	const value = await this.getAsync(key);
-	return value;
-    }
+  async get(key) {
+    const value = await this.getAsync(key);
+    return value;
+  }
 
-    async set(key, value, duration) {
-	this.client.set(key, value);
-	this.client.expire(key, duration);
-    }
+  async set(key, value, duration) {
+    this.client.set(key, value);
+    this.client.expire(key, duration);
+  }
 
-    async del(key) {
-	this.client.del(key);
-    }
+  async del(key) {
+    this.client.del(key);
+  }
 }
+
 const redisClient = new RedisClient();
 export default redisClient;
